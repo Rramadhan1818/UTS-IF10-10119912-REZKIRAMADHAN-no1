4c49f0b606d65e008e4b7d003cb5eb799dfd5622
Rramadhan1818 <55430991+Rramadhan1818@users.noreply.github.com>
2020-12-04 21:04:07 +0700

    first add

---
+++ src/uts/if10/pkg10119912/rezkiramadhan/no1/UTSIF1010119912REZKIRAMADHANNo1.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uts.if10.pkg10119912.rezkiramadhan.no1;
+
+import java.util.Scanner;
+/**
+ *
+ * @author 
+ * NAMA  : Rezki Ramadhan
+ * KELAS : IF10K
+ * NIM  `: 10119912
+ * 
+ */
+public class UTSIF1010119912REZKIRAMADHANNo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Age age = new Age(2018);
+
+        Scanner scn = new Scanner(System.in);
+        System.out.print("Masukkan Tahun lahir anda : ");
+        age.setYearBirth(scn.nextInt());
+        
+        System.out.println("======Hasil Perhitungan Umur======");
+        System.out.println("Tahun Lahir Anda : " + age.getYearBirth(0));
+        System.out.println("Hari ini Tahun : " + age.getYearNow());
+        System.out.println("Umur kamu sampai hari ini adalah "
+                + age.hitungUmur()+ " tahun");
+        System.out.println("Tandanya Kamu : "+ age.tandanyaKamu(age.hitungUmur()));
+    }
+    
+}
